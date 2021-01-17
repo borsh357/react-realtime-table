d32c22e4fbe5bb72b2a5d032989487d9351f3567
borsh357 <alexey.cher357@gmail.com>
2021-01-17 19:00:52 +0300

    added validation for input data

--- src/reducers/editTableReducer.js
+++ src/reducers/editTableReducer.js
@@ -4,6 +4,10 @@ export default function editTableReducer(state = initialState, action) {
   let newState
   switch (action.type) {
     case 'EDIT_QUANTITY':
+      if (!Number(action.payload.quantity)) {
+        alert('Enter valid number!')
+        return state
+      }
       newState = state.map((row) => {
         if (row.id === action.payload.id) {
           row.quantity = Number(action.payload.quantity)
@@ -12,6 +16,10 @@ export default function editTableReducer(state = initialState, action) {
       })
       return newState
     case 'EDIT_PRICE':
+      if (!Number(action.payload.price)) {
+        alert('Enter valid number!')
+        return state
+      }
       newState = state.map((row) => {
         if (row.id === action.payload.id) {
           row.priceForOne = Number(action.payload.price)
