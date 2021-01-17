3f408739df5b244814c5de9187f20ab677ac593f
borsh357 <alexey.cher357@gmail.com>
2021-01-17 18:53:51 +0300

    added TotalPriceRow component, updated readme

--- src/Components/TotalPriceRow.jsx
+++ src/Components/TotalPriceRow.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './TotalPriceRow.scss'
+import { useSelector } from 'react-redux'
 
 export default function TotalPriceRow() {
-  return <div className="table-totalPrice">Общая стоимость: 5000</div>
+  const rows = useSelector((state) => state)
+  let totalSum = 0
+  rows.map((row) => {
+    totalSum += row.quantity * row.priceForOne
+    return totalSum
+  })
+  return <div className="table-totalPrice">Общая стоимость: {totalSum}</div>
 }
